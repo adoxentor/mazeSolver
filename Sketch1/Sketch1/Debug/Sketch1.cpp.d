db0d6cc7908bdd995a596f2082f55a0c65ce6c48
gilad ziv <giladziv1.2@gmail.com>
2018-03-25 22:16:32 +0300

    gilad

--- Sketch1/Sketch1/Debug/Sketch1.cpp.d
+++ Sketch1/Sketch1/Debug/Sketch1.cpp.d
@@ -1,5 +1,5 @@
-C:\Users\IDDO\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp.o: \
- C:\Users\IDDO\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp \
+C:\Users\Gilad\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp.o: \
+ C:\Users\Gilad\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
@@ -11,7 +11,7 @@ C:\Users\IDDO\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp.o: \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Users\IDDO\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/MPU9250_RegisterMap.h \
+ C:\Users\Gilad\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/MPU9250_RegisterMap.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
@@ -19,6 +19,6 @@ C:\Users\IDDO\AppData\Local\Temp\VMBuilds\Sketch1\uno\Debug\Sketch1.cpp.o: \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/Servo.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/avr/ServoTimers.h \
- C:\Users\IDDO\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/SparkFunMPU9250-DMP.h \
- C:\Users\IDDO\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/util/inv_mpu.h \
- C:\Users\IDDO\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/util/inv_mpu_dmp_motion_driver.h
+ C:\Users\Gilad\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/SparkFunMPU9250-DMP.h \
+ C:\Users\Gilad\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/util/inv_mpu.h \
+ C:\Users\Gilad\Documents\Arduino\libraries\SparkFun_MPU-9250-DMP_Arduino_Library-master\src/util/inv_mpu_dmp_motion_driver.h
